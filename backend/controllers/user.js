19e5a2ba21a096480c878438c10102233a5bcd1f
jadefred <97032726+jadefred@users.noreply.github.com>
2022-04-12 23:49:16 +0200

    backend user controller signup function code refactoring

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -3,19 +3,19 @@ const saltRounds = 10;
 
 const User = require("../models/user");
 
-exports.signup = (req, res, next) => {
-  bcrypt
-    .hash(req.body.password, saltRounds)
-    .then((hash) => {
-      const user = new User({
-        email: req.body.email,
-        password: hash,
-      });
+exports.signup = async (req, res, next) => {
+  try {
+    const bcryptPwd = await bcrypt.hash(req.body.password, saltRounds);
+    const user = new User({
+      email: req.body.email,
+      password: bcryptPwd,
+    });
 
-      user
-        .save()
-        .then(() => res.status(201).json({ message: "Utilisateur créé !" }))
-        .catch((error) => res.status(400).json({ message: error }));
-    })
-    .catch((error) => res.status(500).json({ error }));
+    user
+      .save()
+      .then(() => res.status(201).json({ message: "Utilisateur créé !" }))
+      .catch((error) => res.status(400).json({ message: error }));
+  } catch (error) {
+    res.status(500).json({ error });
+  }
 };
