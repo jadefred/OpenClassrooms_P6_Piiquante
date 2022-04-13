27d43069d6b6c2d8bba7ee61c68e660037b16946
jadefred <97032726+jadefred@users.noreply.github.com>
2022-04-13 13:46:50 +0200

    backend controller, routes added login middlewear, and used jwt to generate token

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,9 +1,14 @@
 const bcrypt = require("bcrypt");
 const saltRounds = 10;
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const secretCode = process.env.JWT_SECRET_STRING;
 
 const User = require("../models/user");
 
-exports.signup = async (req, res, next) => {
+exports.signup = async (req, res) => {
   try {
     const bcryptPwd = await bcrypt.hash(req.body.password, saltRounds);
     const user = new User({
@@ -11,11 +16,40 @@ exports.signup = async (req, res, next) => {
       password: bcryptPwd,
     });
 
-    user
-      .save()
-      .then(() => res.status(201).json({ message: "Utilisateur créé !" }))
-      .catch((error) => res.status(400).json({ message: error }));
+    const saved = await user.save();
+    if (!saved) {
+      return res.status.json(400).json({ message: error });
+      //comment je peux le tester pour être sûr que ça marche??
+    }
+    res.status(201).json({ message: "Utilisateur créé !" });
+
+    // user
+    //   .save()
+    //   .then(() => res.status(201).json({ message: "Utilisateur créé !" }))
+    //   .catch((error) => res.status(400).json({ message: error }));
   } catch (error) {
     res.status(500).json({ error });
+  }
+};
+
+exports.login = async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) {
+      return res.status(401).json({ error: "Utilisateur non trouvé !" });
+    }
+
+    const match = await bcrypt.compare(req.body.password, user.password);
+    if (!match) {
+      return res.status(401).json({ error: "Mot de passe incorrect !" });
+    }
+    res.status(200).json({
+      userId: user._id,
+      token: jwt.sign({ userId: user._id }, secretCode, {
+        expiresIn: "24h",
+      }),
+    });
+  } catch (error) {
+    res.status(500).json(error);
   }
 };
