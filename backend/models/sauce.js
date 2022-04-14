a4c763b09397fd63c39e544f7746c929ef06ed30
jadefred <97032726+jadefred@users.noreply.github.com>
2022-04-14 13:08:06 +0200

    backend created model for sauce

---
+++ backend/models/sauce.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const sauceSchema = mongoose.Schema({
+  name: { type: String, require: true },
+  manufacturer: { type: String, require: true },
+  description: { type: String, require: true },
+  mainPepper: { type: String, require: true },
+  imageUrl: { type: String, require: true },
+  heat: { type: Number, require: true },
+  likes: { type: Number, default: 0 },
+  dislikes: { type: Number, default: 0 },
+  usersLiked: [{ type: Schema.Types.ObjectId, ref: "users" }],
+  usersDisliked: [{ type: Schema.Types.ObjectId, ref: "users" }],
+});
+
+module.exports = mongoose.model("Sauce", sauceSchema);
